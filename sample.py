89ac4dd572ad2919e5c360eda1ad449b63795411
tmy310 <mito.motohiro@gmail.com>
2020-03-13 18:24:44 +0900

    add sample code

---
+++ sample.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from libs.sudoku_rule_keeper import SudokuRuleKeeper
+
+strs = ["(1,2,3,4,5,6,7,8,1)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(1,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)","(x,x,x,x,x,x,x,x,x)"]
+
+sudoku_rule_keeper = SudokuRuleKeeper()
+
+
+print(sudoku_rule_keeper.main(strs))
+
